f8bc111c17a2495aae5e3779afd679c121e5501e
oemyner <ilicix@outlook.com>
2021-11-11 15:44:30 +0800

    ss

--- app.py
+++ app.py
@@ -8,10 +8,11 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     return 'Hello World!'
 
+
 @app.route("/", methods=['POST'])
 def user_register():
     return request.json
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",port=6543)
+    app.run(host="0.0.0.0", port=6543)
